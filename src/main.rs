db080870b54fbe975701dd01e43280cc355ab34e
TSO <coding.tso@gmail.com>
2022-04-13 10:46:35 +0800

    feat: parse-nice-int-from-char-problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("hellow, world!");
+fn get_age(age: &str) -> u32 {
+    age.chars().next().unwrap().to_digit(10).unwrap()
 }
+
+#[test]
+fn basic_tests() {
+    assert_eq!(get_age("2 years old"), 2);
+    assert_eq!(get_age("4 years old"), 4);
+    assert_eq!(get_age("5 years old"), 5);
+    assert_eq!(get_age("7 years old"), 7);
+}
+
+fn main() {}
